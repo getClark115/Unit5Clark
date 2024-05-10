5c4f30a14afecf4a7411f26ff9a0ec3e405b3197
Clark Rodriguez Millan <740394@student.dusd.net>
2024-05-10 15:25:23 -0700

    5.3 lesson 6 start
    i need to start lesson 6 of 5.3 and finish game

--- Unit5InterfacePer7/Assets/Course Library/Scripts/GameManager.cs
+++ Unit5InterfacePer7/Assets/Course Library/Scripts/GameManager.cs
@@ -8,14 +8,20 @@ public class GameManager : MonoBehaviour
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
 
+    public bool isGameActive;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
     private int score;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
+        isGameActive = true;
         score = 0;
-        scoreText.text = "Score:" + score;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+
+       
     }
 
     // Update is called once per frame
@@ -25,12 +31,23 @@ public class GameManager : MonoBehaviour
     }
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+
         }
         
+    }
+   public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive=false;
     }
 }
